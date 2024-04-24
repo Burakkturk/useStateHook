065738f5bf291d61c5b81c3e0a24e4c84ec0c0ba
Burakkturk <burak1845turk@icloud.com>
2024-04-24 12:29:51 +0300

    OBJECT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Form from './components/1-forms/Form';
+import FormObject from './components/1-forms/FormObject';
 import KeyboardClipboards from './components/2-keyboardClipboards/KeyboardClipboards';
 import MouseEvents from './components/3-mouseEvents/MouseEvents';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,9 +8,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-    <KeyboardClipboards/>
+    <FormObject/>
+    {/* <KeyboardClipboards/>
     <MouseEvents/>
-    <Form/>
+    <Form/> */}
     </div>
   );
 }
